174fe7483b9567f0a0a1e193f8349b496c42d22e
vasiliyRybin <v196093@gmail.com>
2021-10-18 01:44:19 +0300

    Attempt to implement Repository pattern

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HappyBusProject.Controllers
 {
